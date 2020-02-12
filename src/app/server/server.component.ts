153e34f903d24382cb31be336b1dea1c2a722f84
prnjl02 <40962769+prnjl02@users.noreply.github.com>
2020-02-12 21:55:46 +0530

    12feb2020

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
 selector:'app-server',
-templateUrl:'./server.component.html'
+templateUrl:'./server.component.html',
+styleUrls:['./server.component.css']
 })
 export class ServerComponent{
+    username :string='';
+    showSecret=false;
+    log=[];
+
+    onDisplayDetails(){
+        this.showSecret=!this.showSecret;
+        this.log.push(this.log.length +1);
+    }
 
 }
